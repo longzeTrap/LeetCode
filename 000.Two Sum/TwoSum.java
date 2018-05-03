64100bea65fd146e6e3233ca0cd9250d1fd807d4
longzeTrap <33474915+longzeTrap@users.noreply.github.com>
2018-05-03 15:51:19 +0800

    Update TwoSum.java

--- 000.Two Sum/TwoSum.java
+++ 000.Two Sum/TwoSum.java
@@ -1,13 +1,19 @@
-public int[] twoSum(int[] numbers, int target) {
-    int[] result = new int[2];
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for (int i = 0; i < numbers.length; i++) {
-        if (map.containsKey(target - numbers[i])) {
-            result[1] = i + 1;
-            result[0] = map.get(target - numbers[i]);
-            return result;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+           int[] result = new int[2];
+            Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i =0; i<nums.length; ++i)
+        {
+            if (map.containsKey(target - nums[i]))
+            {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+            }
+            
+            map.put(nums[i], i);
         }
-        map.put(numbers[i], i + 1);
+      
+      return result;  
     }
-    return result;
 }
